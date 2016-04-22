bef887a78a38910d1a8dbb03d0e88ea90da147d6
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-04-22 15:22:53 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -242,7 +242,7 @@ function create_config( )
      "webshot",
      "music";
      },
-    sudo_users = {94746365,184111248},
+    sudo_users = {190840223},
     admin_users = {},
     disabled_channels = {}
   }
